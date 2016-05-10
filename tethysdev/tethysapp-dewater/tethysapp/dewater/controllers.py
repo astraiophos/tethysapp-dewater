7fd95e1be1c001a49bb3cc703ae664ce2a7dc87e
astraiophos <jacobbfull@gmail.com>
2016-05-10 16:29:30 -0600

    Updated Mapping components

--- tethysdev/tethysapp-dewater/tethysapp/dewater/controllers.py
+++ tethysdev/tethysapp-dewater/tethysapp/dewater/controllers.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from tethys_sdk.gizmos import MapView, MVDraw, MVView, MVLayer, MVLegendClass
 
 @login_required()
 def home(request):
@@ -15,7 +16,37 @@ def tool(request):
     """
     Controller for the dewatering tool
     """
-    context = {'page_id' : '1'}
+    # Define view options
+    view_options = MVView(
+        projection='EPSG:4326',
+        center=[-100, 40],
+        zoom=3.5,
+        maxZoom=18,
+        minZoom=2
+    )
+
+    # Define drawing options
+    drawing_options = MVDraw(
+        controls=['Box', 'Point', 'Move', 'Modify'],
+        initial='Box',
+        output_format='WKT'
+    )
+
+    # Define map view options
+    map_view_options = MapView(
+            height='600px',
+            width='100%',
+            controls=['ZoomSlider', 'Rotate', 'FullScreen',
+                      {'MousePosition': {'projection': 'EPSG:4326'}},
+                      {'ZoomToExtent': {'projection': 'EPSG:4326', 'extent': [-130, 22, -65, 54]}}],
+            layers=[],
+            view=view_options,
+            basemap='OpenStreetMap',
+            draw=drawing_options,
+            legend=True
+    )
+
+    context = {'page_id' : '1', 'map_view_options': map_view_options}
 
     return render(request, 'dewater/DewateringTool.html', context)
 
--- tethysdev/tethysapp-dewater/tethysapp/dewater/model.py
+++ tethysdev/tethysapp-dewater/tethysapp/dewater/model.py
@@ -1,1 +1,5 @@
 # Put your persistent store models in this file
+# from owslib.wps import GMLMultiPolygonFeatureCollection
+#
+#
+# process_id = v.fixeddistancebuffer
