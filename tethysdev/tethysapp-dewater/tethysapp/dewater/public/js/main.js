7214208bbdcbc330cf985b4cb6b5638c8fa637d1
astraiophos <jacobbfull@gmail.com>
2016-06-02 11:36:19 -0600

    Fixed the coordinate arrays

--- tethysdev/tethysapp-dewater/tethysapp/dewater/public/js/main.js
+++ tethysdev/tethysapp-dewater/tethysapp/dewater/public/js/main.js
@@ -6,6 +6,7 @@ var numFeat = [];
 //counters for building arrays
 var i = 0;
 var c = 0;
+var counter = 0;
 
 function listen() {
 map = TETHYS_MAP_VIEW.getMap();
@@ -60,7 +61,9 @@ var wYCoords = [];
 
 //Split the coordinate arrays into separate X and Y coordinate arrays
 
-var i = 0;
+i = 0;
+c = 0;
+counter = 0;
 
 pCoords = pCoords.split(",");
 console.log(pCoords);
@@ -69,53 +72,64 @@ console.log(wCoords);
 
 do {
     if (isOdd(i) === false){
-        pXCoords[i] = (pCoords[i]);
+        pXCoords[c] = parseFloat(pCoords[i]);
         console.log(pXCoords[i]);
         i = i+1;
+        c = c+1;
         }
 
     else if (isOdd(i) === true){
-        pYCoords[i] = (pCoords[i]);
+        pYCoords[counter] = parseFloat(pCoords[i]);
         console.log(pYCoords[i]);
         i = i+1;
+        counter = counter+1;
         }
 }
 while (i<10);
 
-//reinitialize counter
+//reinitialize counters
 i = 0;
+c = 0;
 
 do{
     if (isOdd(i) === false){
-        wXCoords[i] = (wCoords[i]);
+        wXCoords[c] = parseFloat(wCoords[i]);
         console.log(wXCoords[i]);
         i = i+1;
+        c = c+1;
         }
 
     else if (isOdd(i) === true){
-        wYCoords[i] = (wCoords[i]);
+        wYCoords[counter] = parseFloat(wCoords[i]);
         console.log(wYCoords[i]);
         i = i+1;
+        counter = counter+1;
         }
     }
 while (i < wCoords.length);
 
 //This section defines the cell size based on a percentage for the area selected
 //the shortest dimension is what determines the cellsize
-var percent = 0.1;
+var scale = 0.1;
 var cellSide = 0.0;
 
+console.log("Getting cellSide");
+
 if (Math.abs(pXCoords[0]-pXCoords[1]) > Math.abs(pYCoords[0]-pYCoords[1])){
-    cellSide = Math.abs(pXCoords[0]-pXCoords[1])*percent;}
+    cellSide = Math.abs(pXCoords[0]-pXCoords[1])*scale;}
 else if (Math.abs(pXCoords[0]-pXCoords[1]) < Math.abs(pYCoords[0]-pYCoords[1])) {
-    cellSide = Math.abs(pYCoords[0]-pYCoords[1])*percent;}
+    cellSide = Math.abs(pYCoords[0]-pYCoords[1])*scale;}
 else {
-    cellSide = Math.abs(pXCoords[0]-pXCoords[1])*percent;}
+    cellSide = cellSide + Math.abs(pXCoords[0]-pXCoords[1])*scale;}
+
+console.log(cellSide);
 //This section constructs the featurecollection polygons defining the water table elevations
 //Cells are defined at the corners, water table elevation is defined at the center of the cell
 
 var waterTable = [];
 var sum = 0.0;  //This is for summing q*ln(R/r)
+
+console.log("Getting water table");
 
 for (long = pXCoords[0]-cellSide; long < pXCoords[1]+cellSide; long += cellSide) {
     for (lat = pYCoords[0]-cellSide; lat < pYCoords[1]+cellSide; lat += cellSide) {
@@ -135,7 +149,8 @@ for (long = pXCoords[0]-cellSide; long < pXCoords[1]+cellSide; long += cellSide)
                 'properties': {
                     'elevation' : 100,
                 }
-                        })
+                        });
+        console.log("Added a feature");
         }
     }
 
