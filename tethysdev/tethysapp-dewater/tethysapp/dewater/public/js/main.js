5be042d52dfdec524e7fbfd29ce999e8f8fa22ca
astraiophos <jacobbfull@gmail.com>
2016-06-02 16:05:02 -0600

    Rough Draft of dewatering app

--- tethysdev/tethysapp-dewater/tethysapp/dewater/public/js/main.js
+++ tethysdev/tethysapp-dewater/tethysapp/dewater/public/js/main.js
@@ -3,6 +3,16 @@ var map;
 var wells = [];
 var perimeter = [];
 var numFeat = [];
+//variables for watertable calculations
+var wellx = 0;
+var welly = 0;
+var wellr = 0;
+var deltax = 0;
+var deltay = 0;
+var H = 0;
+var Q = 0;
+var sum = 0; //for summing q*ln(R/r)
+var wtElevation = 0;
 //counters for building arrays
 var i = 0;
 var c = 0;
@@ -26,6 +36,26 @@ map = TETHYS_MAP_VIEW.getMap();
 var wells = [];
 var perimeter = [];
 i = 1;
+
+H = dwte.value - bedrock.value;
+if (H < 0){
+    console.log("Your desired elevation is lower than the bedrock elevation");
+    return;}
+
+H = iwte.value - bedrock.value;
+if (H < 0){
+    console.log("Your initial elevation is lower than the bedrock elevation");
+    return;}
+
+H =  iwte.value- dwte.value;
+if (H < 0){
+    console.log("Your desired elevation is higher than the initial elevation");
+    return;}
+
+Q = q.value;
+if (Q < 0){
+    console.log("For dewatering you need to pump water out, please change the sign of your pumping value");
+    return;}
 
 //this reads the number of features found in the map object and verifies that all of the required features are present
 numFeat = map.getLayers().item(1).getSource().getFeatures();
@@ -73,14 +103,14 @@ console.log(wCoords);
 do {
     if (isOdd(i) === false){
         pXCoords[c] = parseFloat(pCoords[i]);
-        console.log(pXCoords[i]);
+        //console.log(pXCoords[i]);
         i = i+1;
         c = c+1;
         }
 
     else if (isOdd(i) === true){
         pYCoords[counter] = parseFloat(pCoords[i]);
-        console.log(pYCoords[i]);
+        //console.log(pYCoords[i]);
         i = i+1;
         counter = counter+1;
         }
@@ -90,23 +120,26 @@ while (i<10);
 //reinitialize counters
 i = 0;
 c = 0;
+counter = 0;
 
 do{
     if (isOdd(i) === false){
         wXCoords[c] = parseFloat(wCoords[i]);
-        console.log(wXCoords[i]);
+        //console.log(wXCoords[i]);
         i = i+1;
         c = c+1;
         }
 
     else if (isOdd(i) === true){
         wYCoords[counter] = parseFloat(wCoords[i]);
-        console.log(wYCoords[i]);
+        //console.log(wYCoords[i]);
         i = i+1;
         counter = counter+1;
         }
     }
 while (i < wCoords.length);
+console.log("Well 'x' coordinates " + wXCoords);
+console.log("Well 'y' coordinates " + wYCoords);
 
 //This section defines the cell size based on a percentage for the area selected
 //the shortest dimension is what determines the cellsize
@@ -131,8 +164,8 @@ var sum = 0.0;  //This is for summing q*ln(R/r)
 
 console.log("Getting water table");
 
-for (long = pXCoords[0]-cellSide; long < pXCoords[1]+cellSide; long += cellSide) {
-    for (lat = pYCoords[0]-cellSide; lat < pYCoords[1]+cellSide; lat += cellSide) {
+for (long = pXCoords[0]-cellSide; long < pXCoords[2]+cellSide; long += cellSide) {
+    for (lat = pYCoords[0]-cellSide; lat < pYCoords[2]+cellSide; lat += cellSide) {
         waterTable.push({
             'type': 'Feature',
             'geometry': {
@@ -147,13 +180,12 @@ for (long = pXCoords[0]-cellSide; long < pXCoords[1]+cellSide; long += cellSide)
                                ]
                         },
                 'properties': {
-                    'elevation' : 100,
+                    'elevation' : elevationCalc(long,lat,wXCoords,wYCoords),
                 }
                         });
-        console.log("Added a feature");
         }
     }
-
+console.log(waterTable);
 
 var raster_elev = {
     'type': 'FeatureCollection',
@@ -163,12 +195,122 @@ var raster_elev = {
             'name':'EPSG:4326'
             }
     },
-
-
+    'features': waterTable
 };
+addWaterTable(raster_elev);
 }
 
+function elevationCalc(long,lat,wXCoords,wYCoords) {
 
+wellx = 0;
+welly = 0;
+wellr = 0;
+deltax = 0;
+deltay = 0;
+H = iwte.value - bedrock.value
+wtElevation = 0;
+
+i = 0;
+sum = 0;
+do {
+
+    wellx = wXCoords[i];
+    welly = wYCoords[i];
+    Q = q.value/wXCoords.length;
+
+    deltax = Math.abs(long-wellx);
+    deltay = Math.abs(lat-welly);
+
+    wellr = Math.pow((Math.pow(deltax,2) + Math.pow(deltay,2)),0.5);
+
+    //Make sure that we don't divide by zero
+    if (wellr < 0.5){
+        wellr = 0.5;
+        }
+    if (Math.log(500/wellr)<0){
+        sum = sum;
+        }
+    else{
+        sum = sum + Q*Math.log(500/wellr);
+        }
+    i = i+1;
+    }
+while(i < wXCoords.length);
+
+wtElevation = Math.pow((Math.pow(H,2) - sum/(Math.PI*k.value)),0.5)
+
+return wtElevation;
+}
+
+function addWaterTable(raster_elev){
+
+var getStyleColor;
+
+getStyleColor = function(value) {
+    if (value > dwte.value+dwte.value*0.2)
+        return [64,196,64,0.5];
+    else if (value > dwte.value+dwte.value*0.1)
+        return [108,152,64,0.5];
+    else if (value < dwte.value)
+        return [152,108,64,0.5];
+    else
+        return [196,32,32,0.5];
+    };
+
+var defaultStyle = new ol.style.Style({
+    fill: new ol.style.Fill({
+        color: [250,250,250,0.5]
+    }),
+    stroke: new ol.style.Stroke({
+    color: [220,220,220,0.5],
+    width: 1
+    })
+});
+//This will be used to cache the style
+var styleCache = {};
+
+//the style function returns an array of styles for the given feature and resolution
+//Return null to hide the feature
+function styleFunction(feature, resolution){
+    //get the elevation from the feature properties
+    var elevation = feature.get('elevation');
+    //if there is no elevation value or it's one we don't recognize,
+    //return the default style
+    if(!elevation) {
+        return [defaultStyle];
+        }
+    //check the cache and create a new style for the elevation if it's not been created before.
+    if(!styleCache[elevation]){
+        var style_color = getStyleColor(elevation);
+        styleCache[elevation] = new ol.style.Style({
+            fill: new ol.style.Fill({
+                color: style_color
+                }),
+            stroke: defaultStyle.stroke
+            });
+        }
+//at this point, the style for the current level is in the cache so return it as an array
+    return [styleCache[elevation]];
+}
+
+var collection = raster_elev;
+var format = new ol.format.GeoJSON();
+
+var vectorSource = new ol.source.Vector({
+    features: format.readFeatures(collection,
+    {featureProjection:"EPSG:4326"})
+    });
+var vector = new ol.layer.Image({
+        source: new ol.source.ImageVector({
+            source: vectorSource,
+            style: styleFunction
+        })
+    });
+map.addLayer(vector);
+
+
+
+}
 //Create public functions to be called in the controller
 app = {dewater: dewater,
         verify: verify}
